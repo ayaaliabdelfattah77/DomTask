cc8562d571591580e16cc518a913bb378ab2a236
ayaaliabdelfattah77 <ayaali7779922@gmail.com>
2025-11-11 22:06:12 +0200

    modify

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const cardss = [
-  { name: "Aya", paragraph: " The moment you start believing in yourself, you’ve already won half the battle.", image: "777.png" },
-  { name: "Seif", paragraph: "Aim for the impossible and don’t stop until you’ve made it possible.", image: "888.png" },
-  { name: "Hana", paragraph: "The more you invest in your dreams, the more rewarding the harvest will be.", image: "999.png" },
+  {
+    name: "Aya",
+    paragraph:
+      " The moment you start believing in yourself, you’ve already won half the battle.",
+    image: "777.png",
+  },
+  {
+    name: "Seif",
+    paragraph:
+      "Aim for the impossible and don’t stop until you’ve made it possible.",
+    image: "888.png",
+  },
+  {
+    name: "Hana",
+    paragraph:
+      "The more you invest in your dreams, the more rewarding the harvest will be.",
+    image: "999.png",
+  },
 ];
 
 for (const card of cardss) {
-    // الاساسىdiv استدعاء
+  // الاساسىdiv استدعاء
   let cards = document.getElementById("cards");
 
   // الثانى div انشاء
@@ -17,12 +32,14 @@ for (const card of cardss) {
   let myP = document.createElement("p");
   let image = document.createElement("img");
 
-  //كتابه المحتوى ف العناصر 
+  //كتابه المحتوى ف العناصر
   myH2.textContent = card.name;
-  myP.textContent = card.paragraph;
+  //استخدام createtextnode (بتشتغل على document)
+  let text = document.createTextNode(card.paragraph);
+  myP.appendChild(text);
+  
   image.setAttribute("src", card.image);
   image.setAttribute("alt", "picture");
-
 
   //  Cards structure
   cards.appendChild(myDiv);
@@ -30,19 +47,13 @@ for (const card of cardss) {
   myDiv.appendChild(myH2);
   myDiv.appendChild(myP);
   myDiv.appendChild(image);
-
-
 }
 
 // استخدام loop وqueryselectorall لتغيير الخلفيات للكروت مره واحده ولون الخط
-let styleee = document.querySelectorAll(".card")
-for(const card of styleee){
-
-    card.style.backgroundColor ="#f8ef90ff"
-    card.style.color ="#c51aa6ff"
-
+let styleee = document.querySelectorAll(".card");
+for (const card of styleee) {
+  card.style.backgroundColor = "#f8ef90ff";
+  card.style.color = "#c51aa6ff";
 }
 
 console.log(cards);
-
-
